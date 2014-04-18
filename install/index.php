1b17d7e80f0d79578b51dd522e7fe9e060f27736
Oshizu <oshizu@nyaa-nyaa.com>
2014-04-18 13:09:02 -0400

    Initial commit of version 1

--- install/index.php
+++ install/index.php
@@ -1,28 +1,56 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap-community Installer</title>
+<?php
+  $navstep = "Welcome to Bootstrap Community!";
+  include("../includes/installheader.php");
+?>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+<div class="panel panel-default">
+  <div class="panel-body">
+    <h4>Welcome to the Bootstrap Community Installer!</h4>
+    This installer will help setup the Bootstrap framework for your server environment. If you're ready to install, click the button below.
+    <br />
+    <br />
+    <a href="" data-target="#licensebox" data-toggle="modal" class="btn btn-primary btn-sm">Get Started</a>
+    
+	<div class="modal fade" id="licensebox" tabindex="-1" role="dialog" aria-labelledby="licenseBoxLabel" aria-hidden="true">
+	  <div class="modal-dialog">
+	    <div class="modal-content">
+	      <div class="modal-header">
+	        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+	        <h4 class="modal-title" id="licenseBoxLabel">End User License Agreement</h4>
+	      </div>
+	      <div class="modal-body">
+	        <textarea rows="10" class="form-control">
+The MIT License (MIT)
 
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
-  </head>
-  <body>
-    <?php
+Copyright (c) 2014 Oshizu
 
-    ?>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
-</html>
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+	        </textarea>
+	        <p class="text-center">A copy of this license may be obtained from <a href="https://github.com/Oshizu/bootstrap-community" target="_blank">GitHub</a>.</p>
+	      </div>
+	      <div class="modal-footer">
+	        <a href="setup.php" class="btn btn-primary">Agree</a>
+	      </div>
+	    </div>
+	  </div>
+	</div>
+  </div>
+</div>
+
+<?php include("../includes/installfooter.php"); ?>
